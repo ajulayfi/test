01d9786d5ef487aeb5fd9dc0d59ca6fd63eddaf4
Mac <Mac@192.168.0.26>
2018-05-23 17:50:32 -0500

    Forth commit

--- may23/src/may23/Test.java
+++ may23/src/may23/Test.java
@@ -1,5 +1,7 @@
 package may23;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
@@ -7,6 +9,16 @@ public class Test {
 String x = "aaaa";
 int a = 8;
 System.out.println(x+a);
+
+Scanner scan = new Scanner (System.in);
+
+System.out.println("Enter the intger");
+
+int intger = scan.nextInt();
+System.out.println(intger);
+
+scan.close();
+
 
 	}
 
